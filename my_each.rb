7ba1f7f1c0626e520d5716f3c8e0de011012358e
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-12 23:05:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,10 +4,11 @@ def my_each(array) # put argument(s) here
     i = 0
     while i < array.length
         yield array[i]
-    i+=1
+        i+=1
     end
     array
   else
     "BLock empty"
   end
 end
+
